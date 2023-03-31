edc6c58dd97fce4c763239ce901e2c5785f0d94c
andresg19 <andygermain17@gmail.com>
2023-03-31 12:53:55 -0300

    splide skills

--- pages/skills.js
+++ pages/skills.js
@@ -1,50 +1,44 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
+import '@splidejs/react-splide/css/core';
+import { Options, Splide, SplideSlide } from '@splidejs/react-splide';
 import { data } from './assets/infoSkills';
-import "swiper/css"
 
-export default function Skills ()  {
-const infoSkills = data[0].tools;
-    return ( 
-       
-        <>
-        <div className='flex text-white font-light text-xl justify-between'>
-            <img src='./arrowLeft.png' width={30}/>
-             <p>Desplaza hacia cualquier lado</p>
-            <img src='./arrowRigth.png' width={30}/>
-        </div>
 
-     <Swiper className='mt-10'
-     loop
-     spaceBetween={10}
-     slidesPerView={3}
-     >
-    {infoSkills.map((t) => (
-        
-        <SwiperSlide>
-            <div className='grid ml-auto mr-auto w-[80%]'>
-                    <img src={t.img} alt=""  className='w-[100%]'/>
-                    <p className='text-white ml-2'>{t.name}</p>
+
+
+
+
+export default function Skills() {
+  const infoSkills = data[0].tools;
+  return (
+    <div className='bg-white'>
+      <Splide className=''>
+        {infoSkills.map((s) => (
+          <SplideSlide>
+            <div className='text-white p-8 bg-[#18181b] bg-opacity-40 shadow-sm rounded-br-lg rounded-tl-lg shadow-[#070707] w-[80%] mx-auto'>
+            <div className='text-center'>
+            <img src={s.img} className='w-[50%] shadow-lg shadow-[#0000002d] mx-auto'/>
+            <p className='mt-10 mx-auto text-center'>
+              {s.name}
+            </p>
             </div>
-                </SwiperSlide>
-            )
-            )
-        }
-     </Swiper>
-        </>
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </div>
 
-
-    //     <div className='grid grid-cols-4 h-[50vh] mt-12 md:grid-cols-5'>
-    //      {infoSkills.map((t) => {
-    //          return(
-    //              <div className='block ml-auto mr-auto w-[100%] h-[10vh] text-sm text-slate-700 text-center mt-[2%] shadow-sm hover:animate-spin lg:h-[12vh]'>
-    //                  <img className='w-[20%] mt-3 ml-auto mr-auto' src={t.img} alt=""/>
-    //                  <p>{t.name}</p>
-    //              </div>
-    //          )
-    //      })}
-    //  </div>
-     );
+    //      <div className='grid grid-cols-4 h-[50vh] mt-12 md:grid-cols-5'>
+    //       {infoSkills.map((t) => {
+    //           return(
+    //               <div className='block ml-auto mr-auto w-[100%] h-[10vh] text-sm text-slate-700 text-center mt-[2%] shadow-sm hover:animate-spin lg:h-[12vh]'>
+    //                   <img className='w-[20%] mt-3 ml-auto mr-auto' src={t.img} alt=""/>
+    //                   <p>{t.name}</p>
+    //               </div>
+    //           )
+    //       })}
+    //   </div>
+  );
 }
     
 
